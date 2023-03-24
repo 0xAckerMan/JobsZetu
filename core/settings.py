ebf71f539ba0720818560c62016dccff1793ab3f
0xAckerMan <joelkores1@gmail.com>
2023-03-25 00:03:36 +0300

    tURNED OF Debug mode

--- core/settings.py
+++ core/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'kazi.context_processors.kazi_context_processor',
             ],
         },
     },
@@ -140,11 +141,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'kazi/static')
-# ]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'kazi/static')
+]
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'kazi/static/media')
